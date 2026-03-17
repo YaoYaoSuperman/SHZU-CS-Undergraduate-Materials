2067f3d52b1e1ce03e436c708388a735e40edefa
YaoYaoSuperman <2297377878@qq.com>
2026-03-17 18:54:58 +0800

    refactor: 重新整理目录结构，同步最新本地文件

--- 02-实践与项目/实践项目/python_课内习题+实验+期末真题/习题集/第5章习题/习题5.6.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-from  datetime  import*
-
-bd = datetime(2002, 6, 4, 12, 30 ) #
-print(bd)
-print('%s-%s-%s'%(bd.year, bd.month, bd.day)) # %号形式
-print('{0:%Y}-{0:%m}-{0:%d} {0:%a}'.format(bd)) # %a返回周几
-print('{0:%b}.{0:%d}.{0:%Y}'.format(bd)) # %b返回 月份
-print('{0:%d}{1:} {0:%b} {0:%Y}'.format(bd, ['st', 'nd', 'rd', 'th'][bd.day%10-1 if bd.day%10 <= 3 else 3]))
